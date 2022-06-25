b48aa40d68959d6e53ea049518b42ec22cca11d0
Luv sahu <lksahu404@gmail.com>
2022-06-25 16:39:24 +0530

    This code for Search a Key in the Binary Search Tree

---
+++ SearchKey/BinaryTreeNode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    public class BinarySearchTreeNode<k>
+    {
+        public k Key;
+        public BinarySearchTreeNode<k> Right;
+        public BinarySearchTreeNode<k> Left;
+
+        public BinarySearchTreeNode(k key)
+        {
+            this.Key = key;
+            Right = null;
+            Left = null;
+        }
+       
+    }
+   
+}
+
